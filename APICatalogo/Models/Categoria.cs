f94ea315ca9f3761f2da391e02c02035a635e102
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-02-07 13:52:04 -0300

    Configuring DbContext, Connection String and entities relationship

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
 }
 
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -8,5 +8,7 @@ public class Produto
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
 }
 
