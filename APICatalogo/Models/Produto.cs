80151b70aa084f1c377d87eee96bee6193eb5211
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-02-21 21:35:25 -0300

    Fix: Problem object cycle problem and ajust JSON serialization

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
@@ -22,6 +23,7 @@ public class Produto
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     public int CategoriaId { get; set; }
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
 
